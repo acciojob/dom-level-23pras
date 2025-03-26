740022dc6b90e72393a0b05e68edb963dcff817f
Prashant Pal <palprashant465@gmail.com>
2025-03-26 23:01:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function getDomLevel(element) {
+    let level = 0;
+    while (element.parentNode) {
+        level++;
+        element = element.parentNode;
+    }
+    return level;
+}
+
+const element = document.querySelector("#level");
+let level = getDomLevel(element);
+alert(`The level of the element is: ${level}`);
+
+
+
